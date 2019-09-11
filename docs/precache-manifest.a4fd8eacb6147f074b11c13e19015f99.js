c06135421160a0dd7d6e3a238e7e00cbb49be272
claradios <clara.dios.diez@gmail.com>
2019-09-11 10:24:03 +0200

    add docs

---
+++ docs/precache-manifest.a4fd8eacb6147f074b11c13e19015f99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5965b13625f4dea5f1eb25c76d5ef847",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b33e118a060dc4b95c6a",
+    "url": "./static/css/main.cd773751.chunk.css"
+  },
+  {
+    "revision": "b5c6e4a07444c9e3b680",
+    "url": "./static/js/2.6d1f9d9c.chunk.js"
+  },
+  {
+    "revision": "b33e118a060dc4b95c6a",
+    "url": "./static/js/main.58d4b0e4.chunk.js"
+  },
+  {
+    "revision": "7833cb00e43a4f6a0e61",
+    "url": "./static/js/runtime~main.bb0e3b7d.js"
+  }
+]);
