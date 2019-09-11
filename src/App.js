36a9c6c8905f5d26d02a438cd702ab4d2acfdc5d
claradios <clara.dios.diez@gmail.com>
2019-09-11 10:20:41 +0200

    add invisible h1 title and add line to package json folder to publish in the future

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <h1 className="app__title">Lista de Pokemon Cards</h1>
         <PokeList pokemons={this.state.pokemons}/>
       </div>
     );
